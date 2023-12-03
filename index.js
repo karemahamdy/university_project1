bfb17a7b2d46799535c185c867b1b4c509eae0ed
karema-hamdy <https://github.com/karemahamdy>
2023-12-03 20:53:11 +0200

    login & sign up & delete footer and service page

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-var modal = document.getElementById('log-in');
+// log in
+var modal = document.getElementById('modal');
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+}
+//  sign up
+
+var modal = document.getElementById('sign-up');
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    console.log("df")
+    modal.style.display = "none";
+  }
 }
