c6c0cbe40697a7cf15f44e474f78a33481b45d80
karema-hamdy <https://github.com/karemahamdy>
2023-12-17 11:39:33 +0200

    login packup

--- authenticate.php
+++ authenticate.php
@@ -8,7 +8,61 @@ $db = 'member_of _facullty';
 $con = mysqli_connect ($host, $user, $pass, $db);
 
 // التحقق من نجاح الاتصال
-if ($con) {
-    echo'yes it is connect';
+// if ($con) {
+//     echo'yes it is connect';
+// }
+if ( !isset($_POST['username'], $_POST['password']) ) {
+	// Could not get the data that should have been sent.
+	exit('Please fill both the username and password fields!');
+}
+if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
+	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
+	$stmt->bind_param('s', $_POST['username']);
+	$stmt->execute();
+	// Store the result so we can check if the account exists in the database.
+	$stmt->store_result();
+    if ($stmt->num_rows > 0) {
+        $stmt->bind_result($id, $password);
+        $stmt->fetch();
+        // Account exists, now we verify the password.
+        // Note: remember to use password_hash in your registration file to store the hashed passwords.
+        if (password_verify($_POST['password'], $password)) {
+            // Verification success! User has logged-in!
+            // Create sessions, so we know the user is logged in, they basically act like cookies but remember the data on the server.
+            // session_regenerate_id();
+            $_SESSION['loggedin'] = TRUE;
+            $_SESSION['name'] = $_POST['username'];
+            $_SESSION['id'] = $id;
+            echo 'Welcome ' . $_SESSION['name'] . '!';
+        } else {
+            // Incorrect password
+            echo 'Incorrect username and/or password!';
+        }
+    } else {
+        // Incorrect username
+        echo 'Incorrect username and/or password!';
+    }
+
+	$stmt->close();
 }
 ?>
+
+<?php
+// We need to use sessions, so you should always start sessions using the below code.
+session_start();
+// If the user is not logged in redirect to the login page...
+if (!isset($_SESSION['loggedin'])) {
+	header('Location: service.php');
+	exit;
+}
+?>
+
+<?php
+session_start();
+session_destroy();
+// Redirect to the login page:
+header('Location: index.php');
+?>
+
+<!-- end login -->
+
